53195c3aa19aca5e58292430663546bcab8adcce
Rutger Klamer <rutger.klamer@gmail.com>
2025-04-29 00:51:17 +0200

    change tool menu bar

--- 2/js/Toolbar.js
+++ 2/js/Toolbar.js
@@ -1,125 +1,102 @@
 /**********************************
-
+ 
 TOOLBAR CODE
-
+ 
 **********************************/
 
 function Toolbar(loopy){
 
-	const self = this;
+    const self = this;
 
-	// Tools & Buttons
-	const buttons = [];
-	const buttonsByID = {};
-	self.dom = document.getElementById("toolbar");
-	self.addButton = function(options){
+    // Tools & Buttons
+    const buttons = [];
+    const buttonsByID = {};
+    self.dom = document.getElementById("toolbar");
 
-		const id = options.id;
-		const tooltip = options.tooltip;
-		const callback = options.callback;
+    self.addButton = function(options){
+        const id       = options.id;
+        const tooltip  = options.tooltip;
+        const callback = options.callback;
 
-		// Add the button
-		const button = new ToolbarButton(self,{
-			id: id,
-			icon: "css/icons/"+id+".png",
-			tooltip: tooltip,
-			callback: callback
-		});
-		self.dom.appendChild(button.dom);
-		buttons.push(button);
-		buttonsByID[id] = button;
+        // Create the button
+        const button = new ToolbarButton(self, {
+            id       : id,
+            icon     : "css/icons/" + id + ".png",
+            tooltip  : tooltip,
+            callback : callback
+        });
 
-		// Keyboard shortcut!
-		(function(id){
-			subscribe("key/"+id,function(){
-				loopy.ink.reset(); // also CLEAR INK CANVAS
-				buttonsByID[id].callback();
-			});
-		})(id);
+        self.dom.appendChild(button.dom);
+        buttons.push(button);
+        buttonsByID[id] = button;
 
-	};
+        // Keyboard shortcut!
+        (function(id){
+            subscribe("key/" + id, function(){
+                loopy.ink.reset(); // also CLEAR INK CANVAS if you need
+                buttonsByID[id].callback();
+            });
+        })(id);
+    };
 
-	// Select button
-	self.selectButton = function(button){
-		for(let i=0;i<buttons.length;i++){
-			buttons[i].deselect();
-		}
-		button.select();
-	};
+    // Deselect all / select one
+    self.selectButton = function(button){
+        buttons.forEach(b => b.deselect());
+        button.select();
+    };
 
-	// Set Tool
-	self.currentTool = "ink";
-	self.setTool = function(tool){
-		self.currentTool = tool;
-		const name = "TOOL_"+tool.toUpperCase();
-		loopy.tool = Loopy[name];
-		document.getElementById("canvasses").setAttribute("cursor",tool);
-	};
+    // Tool switching
+    self.currentTool = "ink";
+    self.setTool = function(tool){
+        self.currentTool = tool;
+        loopy.tool = Loopy["TOOL_" + tool.toUpperCase()];
+        document.getElementById("canvasses").setAttribute("cursor", tool);
+    };
 
-	// Populate those buttons!
-	self.addButton({
-		id: "ink",
-		tooltip: "PE(N)CIL",
-		callback: function(){
-			self.setTool("ink");
-		}
-	});
-	self.addButton({
-		id: "label",
-		tooltip: "(T)EXT",
-		callback: function(){
-			self.setTool("label");
-		}
-	});
-	self.addButton({
-		id: "drag",
-		tooltip: "MO(V)E",
-		callback: function(){
-			self.setTool("drag");
-		}
-	});
-	self.addButton({
-		id: "erase",
-		tooltip: "(E)RASE",
-		callback: function(){
-			self.setTool("erase");
-		}
-	});
+    // Add the four buttons
+    ["ink","label","drag","erase"].forEach(id => {
+        const tooltipMap = {
+            ink:   "PE(N)CIL",
+            label: "(T)EXT",
+            drag:  "MO(V)E",
+            erase: "(E)RASE"
+        };
+        self.addButton({
+            id:       id,
+            tooltip:  tooltipMap[id],
+            callback: () => self.setTool(id)
+        });
+    });
 
-	// Select button
-	buttonsByID.ink.callback();
-
-	// Hide & Show
+    // Default select “ink”
+    buttonsByID.ink.callback();
 
 }
 
 function ToolbarButton(toolbar, config){
 
-	const self = this;
-	self.id = config.id;
+    const self = this;
+    self.id = config.id;
 
-	// Icon
-	self.dom = document.createElement("div");
-	self.dom.setAttribute("class", "toolbar_button");
-	self.dom.style.backgroundImage = "url('"+config.icon+"')";
+    // Build the div
+    self.dom = document.createElement("div");
+    self.dom.className = "toolbar_button";
+    self.dom.setAttribute("data-tool", config.id);
+    self.dom.style.backgroundImage = "url('" + config.icon + "')";
 
-	// Tooltip!
-	self.dom.setAttribute("data-balloon", config.tooltip);
-	self.dom.setAttribute("data-balloon-pos", "right");
+    // Tooltip
+    self.dom.setAttribute("data-balloon",    config.tooltip);
+    self.dom.setAttribute("data-balloon-pos","right");
 
-	// Selected?
-	self.select = function(){
-		self.dom.setAttribute("selected", "yes");
-	};
-	self.deselect = function(){
-		self.dom.setAttribute("selected", "no");
-	};
+    // Select / Deselect
+    self.select = () =>   self.dom.setAttribute("selected","yes");
+    self.deselect = () => self.dom.setAttribute("selected","no");
 
-	// On Click
-	self.callback = function(){
-		config.callback();
-		toolbar.selectButton(self);
-	};
-	self.dom.onclick = self.callback;
+    // Click
+    self.callback = () => {
+        config.callback();
+        toolbar.selectButton(self);
+    };
+    self.dom.onclick = self.callback;
 
 }
